05cfc9b14b4df8cfd8cb981315f3fa3b12bfe372
Hamza Bouzekri <bouzekrihamza86@gmail.com>
2026-01-19 05:21:30 +0100

    update

--- app/components/providers.tsx
+++ app/components/providers.tsx
@@ -6,12 +6,12 @@ import { getClientMessaging } from "@/firebase/firebase";
 export async function getFcmToken() {
   try {
     const messaging = await getClientMessaging();
-    if (!messaging) return "";
+    if (!messaging) return "m";
         
 
 
     const permission = await Notification.requestPermission();
-    if (permission !== "granted") return "";
+    if (permission !== "granted") return "p";
 
     const token = await getToken(messaging, {
       vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
@@ -19,9 +19,9 @@ export async function getFcmToken() {
 
     
 
-    return token ?? '';
+    return token ?? 't';
   } catch (error) {
     //console.log("FCM Token Error:", error);
-    return "";
+    return "e";
   }
 }
