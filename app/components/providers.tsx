80578582a1a614852884ac3852b98a72710a4e25
Hamza Bouzekri <bouzekrihamza86@gmail.com>
2026-01-19 05:34:19 +0100

    update

--- app/components/providers.tsx
+++ app/components/providers.tsx
@@ -6,12 +6,12 @@ import { getClientMessaging } from "@/firebase/firebase";
 export async function getFcmToken() {
   try {
     const messaging = await getClientMessaging();
-    if (!messaging) return "m";
+    if (!messaging) return "";
         
 
 
-    const permission = await Notification.requestPermission();
-    if (permission !== "granted") return permission;
+    const allowed = await requestNotificationPermission();
+    if (!allowed) return "";
 
     const token = await getToken(messaging, {
       vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
@@ -24,4 +24,24 @@ export async function getFcmToken() {
     //console.log("FCM Token Error:", error);
     return "e";
   }
+}
+
+
+export async function requestNotificationPermission(): Promise<boolean> {
+  if (!("Notification" in window)) return false;
+
+  const permission = Notification.permission;
+
+  if (permission === "granted") return true;
+
+  if (permission === "denied") {
+    alert(
+      "الإشعارات متوقفة.\n" +
+      "اذهب إلى إعدادات التطبيق وفعّل الإشعارات."
+    );
+    return false;
+  }
+
+  const result = await Notification.requestPermission();
+  return result === "granted";
 }
